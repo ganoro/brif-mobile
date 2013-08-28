a35a7c11197492274715fbc664ec453afdd4e1ca
ofer sarid <ofersarid@gmial.com>
2013-08-28 14:18:14 +0300

    ifwm

--- platforms/ios/www/js/main.js
+++ platforms/ios/www/js/main.js
@@ -34,70 +34,104 @@
 	brif.router = new Backbone.Router();
 	Backbone.history.start();
 
-	var googleapi = {
-		authorize: function(options) {
-			var scopes = options.scope.join("+");
-			var deferred = $.Deferred();
-			var authUrl = 'https://accounts.google.com/o/oauth2/auth?' + $.param({
-				response_type: 'code',
-				approval_prompt : 'force',
-				access_type : 'offline',
-				client_id: options.client_id,
-				redirect_uri: options.redirect_uri,
-				scope: scopes
+	var authenticate = function(){
+		var authData = jQuery.parseJSON(localStorage.getItem('authData'));
+		if(!_.isEmpty(authData)) {
+			// save data to user model foe convenient access
+			brif.models.user.set({
+				access_token: authData.access_token,
+				expires_in: authData.expires_in,
+				refresh_token: authData.refresh_token
 			});
-			authUrl = authUrl.replace(/%2B/g, "+");
-			var authWindow = window.open(authUrl, '_blank', 'location=no,toolbar=no');
-			authWindow.addEventListener('loadstart', function(e) {
-				var url = e.url;
-				var code = /\?code=(.+)$/.exec(url);
-				var error = /\?error=(.+)$/.exec(url);
-
-				if (code || error) {
-					authWindow.close();
-				}
-
-				if (code) {
-					$.post('https://accounts.google.com/o/oauth2/token', {
-						code: code[1],
-						client_id: options.client_id,
-						client_secret: options.client_secret,
-						redirect_uri: options.redirect_uri,
-						grant_type: 'authorization_code'
-					}).done(function(data) {
-							deferred.resolve(data);
-							$.post(brif.models.config.get('endPointUrl') + '/auth/mobile-signin',data)
-								.done(function(resp){
-									console.log(resp);
-								})
-								.fail(function(resp){
-
-								});
-							//TODO - send refreshable token to brif backend
-						}).fail(function(response) {
-							alert(response);
-							deferred.reject(response.responseJSON);
-						});
-				} else if (error) {
-					deferred.reject({
-						error: error[1]
-					});
-				}
+			// load main app
+			brif.router.navigate('main');
+			alert(Backbone.history.fragment);
+		} else {
+			signIn({
+				client_id: brif.models.config.get('googleSecret').client_id,
+				redirect_uri: brif.models.config.get('googleSecret').redirect_uris[1],
+				client_secret: brif.models.config.get('googleSecret').client_secret,
+				scope: brif.models.signIn.get('scopes')
 			});
-			return deferred.promise();
 		}
 	};
 
+	var signIn = function(options) {
+		var scopes = options.scope.join("+");
+		var authUrl = 'https://accounts.google.com/o/oauth2/auth?' + $.param({
+			response_type: 'code',
+			approval_prompt : 'force',
+			access_type : 'offline',
+			client_id: options.client_id,
+			redirect_uri: options.redirect_uri,
+			scope: scopes
+		});
+		authUrl = authUrl.replace(/%2B/g, "+");
+		var authWindow = window.open(authUrl, '_blank', 'location=no,toolbar=no');
+		authWindow.addEventListener('loadstart', function(e) {
+			var url = e.url;
+			var code = /\?code=(.+)$/.exec(url);
+			var error = /\?error=(.+)$/.exec(url);
+
+			if (code || error) {
+				authWindow.close();
+			}
+
+			if (code) {
+				$.post('https://accounts.google.com/o/oauth2/token', {
+					code: code[1],
+					client_id: options.client_id,
+					client_secret: options.client_secret,
+					redirect_uri: options.redirect_uri,
+					grant_type: 'authorization_code'
+				}).done(function(data) {
+						// take only what you need
+						var authData = {
+							access_token: data.access_token,
+							expires_in: data.expires_in,
+							refresh_token: data.refresh_token
+						};
+						// Put the authData into storage
+						localStorage.setItem('authData', JSON.stringify(authData));
+						// save data to user model foe convenient access
+						brif.models.user.set({
+							access_token: authData.access_token,
+							expires_in: authData.expires_in,
+							refresh_token: authData.refresh_token
+						});
+						// load main app
+						brif.router.navigate('main');
+						alert(Backbone.history.fragment);
+						$.post(brif.models.config.get('endPointUrl') + '/auth/mobile-signin',data)
+							.done(function(resp){
+								// user authenticated
+								brif.router.navigate('main');
+							})
+							.fail(function(resp){
+								// user failed authentication
+								jQuery.parseJSON(resp);
+								console.log(resp.message);
+								brif.router.navigate('fail');
+							});
+					}).fail(function(response) {
+						alert(response);
+						response = jQuery.parseJSON(response);
+						$('body').html(response.error);
+					});
+			} else if (error) {
+				deferred.reject({
+					error: error[1]
+				});
+			}
+		});
+		return deferred.promise();
+	};
+
 	$(document).on('deviceready', function() {
-		googleapi.authorize({
+		authenticate({
 			client_id: brif.models.config.get('googleSecret').client_id,
-			client_secret: brif.models.config.get('googleSecret').client_secret,
 			redirect_uri: brif.models.config.get('googleSecret').redirect_uris[1],
-			scope: brif.models.signIn.get('scopes')
-		}).done(function(data) {
-			$('body').html('Access Token: ' + data.access_token);
-		}).fail(function(data) {
-			$('body').html(data.error);
+			scopes: brif.models.signIn.get('scopes')
 		});
 	});
 })();
