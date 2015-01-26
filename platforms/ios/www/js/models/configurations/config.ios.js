9c5a254828919e198f7653c0c3d0c3a0c4a55431
Roy Ganor <ganoro@gmail.com>
2015-01-26 00:06:19 -0500

    remove secret

--- platforms/ios/www/js/models/configurations/config.ios.js
+++ platforms/ios/www/js/models/configurations/config.ios.js
@@ -2,17 +2,6 @@
 	brif.modelClass.Config = Backbone.Model.extend({
 		defaults:{
 			googleSecret:{
-				auth_uri:"https://accounts.google.com/o/oauth2/auth",
-				client_secret:"Ucajgf8BPN4fXajscWXLdZ85",
-				token_uri:"https://accounts.google.com/o/oauth2/token",
-				client_email:"",
-				redirect_uris:[
-					"urn:ietf:wg:oauth:2.0:oob",
-					"http://localhost"
-				],
-				client_x509_cert_url:"",
-				client_id:"808248997275-td1l666khkenuda7irdhr27ullu7svps.apps.googleusercontent.com",
-				auth_provider_x509_cert_url:"https://www.googleapis.com/oauth2/v1/certs"
 			},
 			endPointUrl:'http://api.brif.us',
 			apiKey:'AIzaSyDQU5DYKmI1gvk-RGwLJFL0g2r0_Tm5Tko',
